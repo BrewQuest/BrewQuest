489fa3454665106d3360a0a4dc420184be4d2870
Jim Olson <jim.olson.456@gmail.com>
2023-06-14 12:09:34 -0500

    creating

---
+++ src/main/java/com/example/brewquest/BrewQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.brewquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brewquest/BrewQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
