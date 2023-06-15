051f242fdbc77ea9a876a6da303ece24d9b9cce8
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-15 11:43:45 -0500

    Navbar done

--- src/main/java/com/example/brewquest/controllers/HomeController.java
+++ src/main/java/com/example/brewquest/controllers/HomeController.java
@@ -1,14 +1,14 @@
-package com.example.brewquest.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
-    public String home(){
-        return "This is the beginning of our CAPSTONE!";
-    }
-}
+//package com.example.brewquest.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String home(){
+//        return "This is the beginning of our CAPSTONE!";
+//    }
+//}
