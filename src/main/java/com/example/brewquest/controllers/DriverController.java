e0c2443809c17d2ace6acfbb5e809009e0640fa6
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-15 15:07:33 -0500

    driver sign-up completed

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -1,14 +1,38 @@
 package com.example.brewquest.controllers;
 
+
+import com.example.brewquest.models.Driver;
 import com.example.brewquest.repositories.Driver_repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DriverController {
+    private final Driver_repository driverRepository;
 
-private final Driver_repository
+    @Autowired
+    public DriverController(Driver_repository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
 
+    @GetMapping("/signup-driver")
+    public String showSignupForm(Model model) {
+        model.addAttribute("driver", new Driver());
+        return "signup-driver";
+    }
+
+    @PostMapping("/signup-driver")
+    public String processSignupForm(@ModelAttribute("driver") Driver driver, BindingResult result) {
+        if (result.hasErrors()) {
+            return "signup-driver";
+        }
+
+        driverRepository.save(driver);
+        return "redirect:/profile";
+    }
 }
-@GetMapping("/sign-up-driver")
-public String
