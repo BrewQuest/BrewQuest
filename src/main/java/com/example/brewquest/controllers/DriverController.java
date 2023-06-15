608f796800686572275f609e2ebc0f6620bfde38
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-15 14:41:14 -0500

    finished sign-up page for drivers

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -1,12 +1,14 @@
 package com.example.brewquest.controllers;
 
+import com.example.brewquest.repositories.Driver_repository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DriverController {
 
-}
-@GetMapping("Driver")
+private final Driver_repository
 
+}
+@GetMapping("/sign-up-driver")
 public String
--- src/main/java/com/example/brewquest/repositories/Driver_repository.java
+++ src/main/java/com/example/brewquest/repositories/Driver_repository.java
@@ -3,5 +3,5 @@ package com.example.brewquest.repositories;
 import com.example.brewquest.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Driver_repository extends JpaRepository {
+public interface Driver_repository extends JpaRepository <Driver, Long> {
 }
