c51219e2059127a790353aa982b0431bbe19ee81
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-15 14:14:32 -0500

    driver repo

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -1,4 +1,12 @@
 package com.example.brewquest.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DriverController {
+
 }
+@GetMapping("Driver")
+
+public String
---
+++ src/main/java/com/example/brewquest/repositories/Driver_repository.java
@@ -0,0 +1,7 @@
+package com.example.brewquest.repositories;
+
+import com.example.brewquest.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Driver_repository extends JpaRepository {
+}
