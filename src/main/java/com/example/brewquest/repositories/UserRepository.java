fe7e4afde3cb8240bb72b1fef99331c31bbdb937
Daniel <daniel.h.farias2@gmail.com>
2023-06-15 10:38:13 -0500

    updated basic user repo, services

--- src/main/java/com/example/brewquest/repositories/UserRepository.java
+++ src/main/java/com/example/brewquest/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.brewquest.controllers.UserController;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, long> {
+    User findByUsername(String username);
 }
