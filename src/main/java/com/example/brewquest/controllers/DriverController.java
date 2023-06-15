088e9f4f1bdf413a8c4312ae037d63bd16f0fa66
Xavier Francis <james.x.francis9@gmail.com>
2023-06-15 16:43:24 -0500

    user update complete

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -1,14 +1,14 @@
-package com.example.brewquest.controllers;
-
-import com.example.brewquest.repositories.Driver_repository;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class DriverController {
-
-private final Driver_repository
-
-}
-@GetMapping("/sign-up-driver")
-public String
+//package com.example.brewquest.controllers;
+//
+//import com.example.brewquest.repositories.Driver_repository;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class DriverController {
+//
+//private final Driver_repository
+//
+//}
+//@GetMapping("/sign-up-driver")
+//public String
