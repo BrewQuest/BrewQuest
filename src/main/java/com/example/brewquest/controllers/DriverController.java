f72001a90a9311d831baa3a09585445faa351eb5
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-16 11:02:04 -0500

    added the get mapping for edit drivers

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -10,15 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DriverController {
-    private final Driver_repository driverRepository;
+    private final Driver_repository driversDao;
     private final UserRepository usersDao;
 
-    public DriverController(Driver_repository driverRepository, UserRepository usersDao) {
-        this.driverRepository = driverRepository;
+    public DriverController(Driver_repository driversDao, UserRepository usersDao) {
+        this.driversDao = driversDao;
         this.usersDao = usersDao;
     }
 
@@ -38,8 +39,48 @@ public class DriverController {
         driver.setLicensePlateNum(driver.getLicensePlateNum());
         driver.setDriversLicenseNum(driver.getDriversLicenseNum());
 
-        driverRepository.save(driver);
+        driversDao.save(driver);
         return "redirect:/";
     }
+
+    @GetMapping("/driver/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        if (driversDao.findById(id).isPresent()) {
+            Driver driverToEdit = driversDao.findById(id).get();
+            model.addAttribute("driver", driverToEdit);
+        }
+        return "users/profile";
+    }
+
+    @GetMapping("/driver/{id}/edit")
+    public String updateDriver(@ModelAttribute Driver driver) {
+        User user = usersDao.findById(1L).get();
+        driver.setUser(user);
+        driver.setCarMake(driver.getCarMake());
+        driver.setCarModel(driver.getCarModel());
+        driver.setLicensePlateNum(driver.getLicensePlateNum());
+        driver.setDriversLicenseNum(driver.getDriversLicenseNum());
+    }
 }
+
+@GetMapping("/driver/{id}/delete")
+
+//@PostMapping("/posts/{id}/delete")
+//    public String deletePost (@PathVariable long id){
+//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        Post postToDelete = postsDao.findById(id).get();
+//
+//        if(loggedInUser.getId() == postToDelete.getUser().getId()) {
+//            postsDao.deleteById(id);
+//
+//        }
+//        return "redirect:/posts";
+//    @PostMapping("/posts/{id}/edit")
+//    public String updatePost(@ModelAttribute Post newPost) {
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        newPost.setUser(user);
+//        postsDao.save(newPost);
+//        return "redirect:/posts";
+//    }
+    }
 
