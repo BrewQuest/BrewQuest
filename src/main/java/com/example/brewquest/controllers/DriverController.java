4081adba42736d961f2229a9870f17fc2c5287e5
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-16 11:21:02 -0500

    added the delete driver and edit driver

--- src/main/java/com/example/brewquest/controllers/DriverController.java
+++ src/main/java/com/example/brewquest/controllers/DriverController.java
@@ -8,10 +8,7 @@ import com.example.brewquest.repositories.Driver_repository;
 import com.example.brewquest.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DriverController {
@@ -54,33 +51,22 @@ public class DriverController {
 
     @GetMapping("/driver/{id}/edit")
     public String updateDriver(@ModelAttribute Driver driver) {
-        User user = usersDao.findById(1L).get();
+        User user = usersDao.findById(driver.getId()).get();
         driver.setUser(user);
         driver.setCarMake(driver.getCarMake());
         driver.setCarModel(driver.getCarModel());
         driver.setLicensePlateNum(driver.getLicensePlateNum());
         driver.setDriversLicenseNum(driver.getDriversLicenseNum());
+        return "users/profile";
+    }
+
+
+    @DeleteMapping("/drivers/{id}/delete")
+    public String deleteDriver(@PathVariable("id") Long id) {
+        driversDao.deleteById(id);
+        return "users/profile";
     }
 }
 
-@GetMapping("/driver/{id}/delete")
 
-//@PostMapping("/posts/{id}/delete")
-//    public String deletePost (@PathVariable long id){
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        Post postToDelete = postsDao.findById(id).get();
-//
-//        if(loggedInUser.getId() == postToDelete.getUser().getId()) {
-//            postsDao.deleteById(id);
-//
-//        }
-//        return "redirect:/posts";
-//    @PostMapping("/posts/{id}/edit")
-//    public String updatePost(@ModelAttribute Post newPost) {
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        newPost.setUser(user);
-//        postsDao.save(newPost);
-//        return "redirect:/posts";
-//    }
-    }
 
