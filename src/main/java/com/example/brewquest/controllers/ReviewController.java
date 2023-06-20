65feace4861151bf9362613b843d0e7ac02a0032
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-20 11:51:09 -0500

    made the create review controller

--- src/main/java/com/example/brewquest/controllers/ReviewController.java
+++ src/main/java/com/example/brewquest/controllers/ReviewController.java
@@ -1,9 +1,14 @@
 package com.example.brewquest.controllers;
 
+import com.example.brewquest.models.Review;
+import com.example.brewquest.models.User;
 import com.example.brewquest.repositories.ReviewRepository;
 import com.example.brewquest.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReviewController {
@@ -14,6 +19,22 @@ public class ReviewController {
         ReviewDaos = reviewDaos;
         UsersDaos = usersDaos;
     }
-    @GetMapping("create-review")
+    @GetMapping("/create-review")
+    public String showCreateForm(Model model){
+    model.addAttribute("review", new Review());
+    return "/Reviews/Create-Review";
+    }
+@PostMapping("/create-review")
+    public String CreateFormProcess(@ModelAttribute Review review){
+    User user = UsersDaos.findById(1L).get();
+    review.setUser(user);
+    review.setBreweryId(review.getBreweryId());
+    review.setRating(review.getRating());
+    review.setDescription(review.getDescription());
+    review.setPassengers(review.getPassengers());
+
+    ReviewDaos.save(review);
+    return "redirect:/";
+}
 
 }
