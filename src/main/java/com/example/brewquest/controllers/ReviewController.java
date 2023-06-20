e13760e06062dad1ae668c955c9ced3205555bdd
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-20 14:13:10 -0500

    Review controller and review edit html

--- src/main/java/com/example/brewquest/controllers/ReviewController.java
+++ src/main/java/com/example/brewquest/controllers/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,5 +37,33 @@ public class ReviewController {
     ReviewDaos.save(review);
     return "redirect:/";
 }
+@GetMapping("/review/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model){
+        if (ReviewDaos.findById(id).isPresent()) {
+            Review reviewtoEdit = ReviewDaos.findById(id).get();
+            model.addAttribute("driver", reviewtoEdit);
+        }
+        return "/Reviews/Edit-Review";
+}
 
 }
+// @GetMapping("/driver/{id}/edit")
+//    public String showEditForm(@PathVariable long id, Model model) {
+//        if (driversDao.findById(id).isPresent()) {
+//            Driver driverToEdit = driversDao.findById(id).get();
+//            model.addAttribute("driver", driverToEdit);  // Make sure the attribute name is "driver"
+//        }
+//        return "Driver/edit-driver";
+//    }
+//
+//    @PostMapping("/driver/{id}/edit")
+//    public String updateDriver(@PathVariable long id, @ModelAttribute Driver driver) {
+//        User user = usersDao.findById(id).get();
+//        driver.setUser(user);
+//        driver.setCarMake(driver.getCarMake());
+//        driver.setCarModel(driver.getCarModel());
+//        driver.setLicensePlateNum(driver.getLicensePlateNum());
+//        driver.setDriversLicenseNum(driver.getDriversLicenseNum());
+//        driversDao.save(driver);
+//        return "users/profile";
+//    }
