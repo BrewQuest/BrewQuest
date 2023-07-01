d7af34455c82c6956afdbd335faddb7ad9072105
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-30 19:01:43 -0500

    fixed

--- src/main/java/com/example/brewquest/repositories/FriendsRepository.java
+++ src/main/java/com/example/brewquest/repositories/FriendsRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface FriendsRepository extends JpaRepository <Friend, Long> {
     Friend findByUserAndFriend(User loggedInUser, User user);
 
+    List<Friend> findByUser(User loggedInUser);
 }
